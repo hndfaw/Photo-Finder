5ba873dbc116abfaa1b4bbeb3a33fffd3f3d5577
“Hindreen” <hndfaw@gmail.com>
2019-09-14 10:55:50 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <p>{{ two }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+
+  data() {
+    return {
+      msg: 'Hi vueeee.....',
+      two: 'two p tagssss'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
