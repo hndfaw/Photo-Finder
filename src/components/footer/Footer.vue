be2ce69ab0aaae4fdff828275b4bed5928f48ccb
“Hindreen” <hndfaw@gmail.com>
2019-09-15 20:10:15 -0600

    Activate last and first page buttons

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <footer id="footer" v-bind:class="{'hide-footer': !props.pagesFound}">
     <div class="pages-footer">
-      <button @click="submitChangePage('first')" class="first-last-btn">First</button>
+      <button @click="submitChangePage('first')" class="first-last-btn first-btn">First</button>
       <button @click="submitChangePage('decrement')" class="prev-next-btn">Previous</button>
       <p class="number-of-pages">{{props.currentPage + '/'+ props.pagesFound}}</p>
       <button @click="submitChangePage('increment')" class="prev-next-btn">Next</button>
-      <button @click="submitChangePage('last')" class="first-last-btn">Last</button>
+      <button @click="submitChangePage('last')" class="first-last-btn last-btn">Last</button>
     </div>
   </footer>
 </template>
