4bc0cdb8765c305a4fa199cedd48cf816ce9306b
“Hindreen” <hndfaw@gmail.com>
2019-09-16 13:14:54 -0600

    Solved api key issue using .env

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,15 +1,15 @@
 export const fetchImages = (params, currentPage) => {
-    console.log(params.orientation, currentPage)
- return fetch(`https://api.unsplash.com/search/photos?client_id=${"606f0c4b04ddddb42d2bfd6d320a5080b8ec60c216405ad12959b4649b7524e4"}&page=${currentPage}&per_page=100&orientation=${params.orientation}&query=${params.searchTerm}`)
+ return fetch(`https://api.unsplash.com/search/photos?client_id=${process.env.VUE_APP_ACCESS_KEY}&page=${currentPage}&per_page=100&orientation=${params.orientation}&query=${params.searchTerm}`)
     .then(res => res.json())
     .then(data => data)
     .catch(error => console.log(error))
 }
 
 export const fetchRandomImages = () => {
-    return fetch(`https://api.unsplash.com/photos/random?client_id=${"606f0c4b04ddddb42d2bfd6d320a5080b8ec60c216405ad12959b4649b7524e4"}&count=30&orientation=landscape`)
+    return fetch(`https://api.unsplash.com/photos/random?client_id=${process.env.VUE_APP_ACCESS_KEY}&count=30&orientation=landscape`)
        .then(res => res.json())
        .then(data => data)
        .catch(error => console.log(error))
+       
    }
 
